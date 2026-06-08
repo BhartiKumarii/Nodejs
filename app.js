fa2f69d595d8b7eeaa053d4648154ea7e9aab55e
bhartivishwakarma <bhartiv12301@gmail.com>
2026-06-08 23:54:26 +0530

    add app.js with a whimsical sentence using variables

---
+++ app.js
@@ -0,0 +1,8 @@
+let noun1 = 'person';
+let adjective = 'silly';
+let noun2 = 'Fellow';
+let verb = 'Watch scenery';
+let noun3 = 'fish'; 
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
